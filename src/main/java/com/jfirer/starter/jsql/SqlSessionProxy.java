9396944ae411b1af57f873c6fc55cd225f1e18c2
linbin <495561397@qq.com>
2025-09-15 09:58:32 +0800

    需要检查

--- src/main/java/com/jfirer/starter/jsql/SqlSessionProxy.java
+++ src/main/java/com/jfirer/starter/jsql/SqlSessionProxy.java
@@ -29,78 +29,79 @@ public class SqlSessionProxy implements SqlSession
     @Override
     public <T> void batchInsert(Collection<T> collection, int batchSize)
     {
+        sessionFactory.openSession().batchInsert(collection, batchSize);
     }
 
     @Override
     public <T> T findOne(QueryModel model)
     {
-        return null;
+        return sessionFactory.openSession().findOne(model);
     }
 
     @Override
     public <T> List<T> findList(QueryModel model)
     {
-        return List.of();
+        return sessionFactory.openSession().findList(model);
     }
 
     @Override
     public Page findListByPage(QueryModel model)
     {
-        return null;
+        return sessionFactory.openSession().findListByPage(model);
     }
 
     @Override
     public int count(Model model)
     {
-        return 0;
+        return sessionFactory.openSession().count(model);
     }
 
     @Override
     public int execute(Model model)
     {
-        return 0;
+        return sessionFactory.openSession().execute(model);
     }
 
     @Override
     public <T> int save(T entity)
     {
-        return 0;
+        return sessionFactory.openSession().save(entity);
     }
 
     @Override
     public <T> int update(T entity)
     {
-        return 0;
+        return sessionFactory.openSession().update(entity);
     }
 
     @Override
     public <T> int insert(T entity)
     {
-        return 0;
+        return sessionFactory.openSession().insert(entity);
     }
 
     @Override
     public int execute(String sql, List<Object> params)
     {
-        return 0;
+        return sessionFactory.openSession().execute(sql, params);
     }
 
     @Override
     public String insertReturnPk(String sql, List<Object> params, TableEntityInfo.ColumnInfo pkInfo)
     {
-        return "";
+        return sessionFactory.openSession().insertReturnPk(sql, params, pkInfo);
     }
 
     @Override
     public <T> T query(String sql, ResultSetTransfer transfer, List<Object> params)
     {
-        return null;
+        return sessionFactory.openSession().query(sql, transfer, params);
     }
 
     @Override
     public <T> List<T> queryList(String sql, ResultSetTransfer transfer, List<Object> params)
     {
-        return List.of();
+        return sessionFactory.openSession().queryList(sql, transfer, params);
     }
 
     @Override
@@ -112,6 +113,6 @@ public class SqlSessionProxy implements SqlSession
     @Override
     public Connection getConnection()
     {
-        return null;
+        return sessionFactory.openSession().getConnection();
     }
 }
