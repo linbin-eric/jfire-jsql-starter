3d1d91f3118a45a93f3e3b6b1a2b27fe567437fc
linbin <495561397@qq.com>
2026-02-06 17:42:19 +0800

    综述: 重构 MapperFactory 支持 Repository 接口自动生成 CRUD 方法 - 修改 getBean 方法，将方法处理方式从循环类继承层次改为使用 getMethods() 获取所有公共方法，并增加检查确保是接口类型 - 新增 findAllInterface 方法，递归查找接口继承链中的所有接口类型 - 新增 findReposositoryInterface 方法，查找并获取 Repository 泛型接口的泛型参数类型 - 当检测到接口继承 Repository 时，自动生成以下 CRUD 方法：findOne（查询单条）、findList（两重载，查询列表和分页查询）、count（计数）、delete（删除）、insert（插入）、save（保存）、update（更新）

--- src/main/java/cc/jfire/starter/jsql/MapperFactory.java
+++ src/main/java/cc/jfire/starter/jsql/MapperFactory.java
@@ -1,5 +1,6 @@
 package cc.jfire.starter.jsql;
 
+import cc.jfire.baseutil.STR;
 import cc.jfire.baseutil.reflect.ReflectUtil;
 import cc.jfire.baseutil.smc.SmcHelper;
 import cc.jfire.baseutil.smc.compiler.CompileHelper;
@@ -8,10 +9,18 @@ import cc.jfire.baseutil.smc.model.MethodModel;
 import cc.jfire.jfire.core.bean.BeanDefinition;
 import cc.jfire.jfire.core.beanfactory.BeanFactory;
 import cc.jfire.jsql.SessionFactory;
+import cc.jfire.jsql.mapper.Repository;
+import cc.jfire.jsql.metadata.Page;
+import cc.jfire.jsql.model.Param;
 import cc.jfire.jsql.session.SqlSession;
 import lombok.Data;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Data
 public class MapperFactory implements BeanFactory
@@ -27,38 +36,58 @@ public class MapperFactory implements BeanFactory
         classModel.addImport(SqlSession.class);
         String   referenceName = SmcHelper.getReferenceName(descriptorClass, classModel);
         Class<?> type          = descriptorClass;
-        while (type != Object.class && type!=null)
+        if (type.isInterface() == false)
         {
-            for (Method each : type.getDeclaredMethods())
+            throw new IllegalArgumentException();
+        }
+        for (Method each : type.getMethods())
+        {
+            if (each.getDeclaringClass() == Repository.class)
             {
-                MethodModel   methodModel = new MethodModel(each, classModel);
-                StringBuilder body        = new StringBuilder();
-                body.append("SqlSession sqlSession = sessionFactory.openSession();\r\n");
-                if (each.getReturnType() == void.class)
-                {
-                    ;
-                }
-                else
-                {
-                    body.append("return ");
-                }
-                body.append("sqlSession.getMapper(").append(referenceName).append(".class).").append(each.getName()).append('(');
-                Class<?>[] parameterTypes = each.getParameterTypes();
-                boolean    hasComma       = false;
-                for (int i = 0; i < parameterTypes.length; i++)
-                {
-                    hasComma = true;
-                    body.append("$").append(i).append(',');
-                }
-                if (hasComma)
-                {
-                    body.setLength(body.length() - 1);
-                }
-                body.append(");\r\n");
-                methodModel.setBody(body.toString());
-                classModel.putMethodModel(methodModel);
+                continue;
             }
-            type = type.getSuperclass();
+            MethodModel   methodModel = new MethodModel(each, classModel);
+            StringBuilder body        = new StringBuilder();
+            body.append("SqlSession sqlSession = sessionFactory.openSession();\r\n");
+            if (each.getReturnType() == void.class)
+            {
+                ;
+            }
+            else
+            {
+                body.append("return ");
+            }
+            body.append("sqlSession.getMapper(").append(referenceName).append(".class).").append(each.getName()).append('(');
+            Class<?>[] parameterTypes = each.getParameterTypes();
+            boolean    hasComma       = false;
+            for (int i = 0; i < parameterTypes.length; i++)
+            {
+                hasComma = true;
+                body.append("$").append(i).append(',');
+            }
+            if (hasComma)
+            {
+                body.setLength(body.length() - 1);
+            }
+            body.append(");\r\n");
+            methodModel.setBody(body.toString());
+            classModel.putMethodModel(methodModel);
+        }
+        Set<Class> set = new HashSet<>();
+        findAllInterface(type, set);
+        if (set.contains(Repository.class))
+        {
+            Type     genericSuperclass  = findReposositoryInterface(type);
+            Type     actualTypeArgument = ((ParameterizedType) genericSuperclass).getActualTypeArguments()[0];
+            Class<?> repositoryClass    = (Class<?>) actualTypeArgument;
+            addFindOneMethod(classModel, repositoryClass, descriptorClass);
+            addFindListMethod(classModel, repositoryClass, descriptorClass);
+            addFindListPageMethod(classModel, repositoryClass, descriptorClass);
+            addCountMethod(classModel, descriptorClass);
+            addDeleteMethod(classModel, descriptorClass);
+            addInsertMethod(classModel, repositoryClass, descriptorClass);
+            addSaveMethod(classModel, repositoryClass, descriptorClass);
+            addUpdateMethod(classModel, repositoryClass, descriptorClass);
         }
         try
         {
@@ -72,6 +101,176 @@ public class MapperFactory implements BeanFactory
             ReflectUtil.throwException(e);
             return null;
         }
+    }
+
+    private void findAllInterface(Class<?> type, Set<Class> set)
+    {
+        set.add(type);
+        for (Type genericInterface : type.getGenericInterfaces())
+        {
+            if (genericInterface instanceof Class a)
+            {
+                findAllInterface(a, set);
+            }
+            else if (genericInterface instanceof ParameterizedType a)
+            {
+                findAllInterface((Class<?>) a.getRawType(), set);
+            }
+        }
+    }
+
+    private Type findReposositoryInterface(Type type)
+    {
+        if (type instanceof Class<?> a)
+        {
+            for (Type genericInterface : a.getGenericInterfaces())
+            {
+                if (genericInterface instanceof Class<?> b)
+                {
+                    Type result = findReposositoryInterface(b);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+                else if (genericInterface instanceof ParameterizedType b)
+                {
+                    if (((Class) b.getRawType()) == Repository.class)
+                    {
+                        return b;
+                    }
+                    Type result = findReposositoryInterface(b.getRawType());
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+        }
+        else if (type instanceof ParameterizedType a)
+        {
+            if (a.getRawType() == Repository.class)
+            {
+                return a;
+            }
+            else
+            {
+                return findReposositoryInterface(a.getRawType());
+            }
+        }
+        return null;
+    }
+
+    private void addFindOneMethod(ClassModel classModel, Class<?> repositoryClass, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("findOne");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(repositoryClass);
+        methodModel.setParamterTypes(Param.class);
+        methodModel.setParamterNames("param");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).findOne(param);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addFindListMethod(ClassModel classModel, Class<?> repositoryClass, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("findList");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(List.class);
+        methodModel.setParamterTypes(Param.class);
+        methodModel.setParamterNames("param");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).findList(param);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addFindListPageMethod(ClassModel classModel, Class<?> repositoryClass, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("findList");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(List.class);
+        methodModel.setParamterTypes(Param.class, Page.class);
+        methodModel.setParamterNames("param", "page");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).findList(param, page);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addCountMethod(ClassModel classModel, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("count");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(int.class);
+        methodModel.setParamterTypes(Param.class);
+        methodModel.setParamterNames("param");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).count(param);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addDeleteMethod(ClassModel classModel, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("delete");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(int.class);
+        methodModel.setParamterTypes(Param.class);
+        methodModel.setParamterNames("param");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).delete(param);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addInsertMethod(ClassModel classModel, Class<?> repositoryClass, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("insert");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(int.class);
+        methodModel.setParamterTypes(repositoryClass);
+        methodModel.setParamterNames("entity");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).insert(entity);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addSaveMethod(ClassModel classModel, Class<?> repositoryClass, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("save");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(int.class);
+        methodModel.setParamterTypes(repositoryClass);
+        methodModel.setParamterNames("entity");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).save(entity);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
+    }
+
+    private void addUpdateMethod(ClassModel classModel, Class<?> repositoryClass, Class<?> descriptorClass)
+    {
+        MethodModel methodModel = new MethodModel(classModel);
+        methodModel.setMethodName("update");
+        methodModel.setAccessLevel(MethodModel.AccessLevel.PUBLIC);
+        methodModel.setReturnType(int.class);
+        methodModel.setParamterTypes(repositoryClass);
+        methodModel.setParamterNames("entity");
+        methodModel.setBody(STR.format("""
+                                               SqlSession sqlSession = sessionFactory.openSession();
+                                               return sqlSession.getMapper({}.class).update(entity);\r\n""", SmcHelper.getReferenceName(descriptorClass, classModel)));
+        classModel.putMethodModel(methodModel);
     }
 
     public static class MapperProxy
