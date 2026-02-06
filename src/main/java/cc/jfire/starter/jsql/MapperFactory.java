4a46c2bb7ba3b64204aad0c93051f1314952952e
linbin <495561397@qq.com>
2026-02-06 16:10:14 +0800

    修正 MapperFactory，对类的处理，循环所有的方法

--- src/main/java/cc/jfire/starter/jsql/MapperFactory.java
+++ src/main/java/cc/jfire/starter/jsql/MapperFactory.java
@@ -25,35 +25,40 @@ public class MapperFactory implements BeanFactory
         Class<?>   descriptorClass = beanDefinition.getType();
         ClassModel classModel      = new ClassModel(descriptorClass.getSimpleName() + "$Mapper", MapperProxy.class, descriptorClass);
         classModel.addImport(SqlSession.class);
-        String referenceName = SmcHelper.getReferenceName(descriptorClass, classModel);
-        for (Method each : descriptorClass.getDeclaredMethods())
+        String   referenceName = SmcHelper.getReferenceName(descriptorClass, classModel);
+        Class<?> type          = descriptorClass;
+        while (type != Object.class)
         {
-            MethodModel   methodModel = new MethodModel(each, classModel);
-            StringBuilder body        = new StringBuilder();
-            body.append("SqlSession sqlSession = sessionFactory.openSession();\r\n");
-            if (each.getReturnType() == void.class)
+            for (Method each : type.getDeclaredMethods())
             {
-                ;
+                MethodModel   methodModel = new MethodModel(each, classModel);
+                StringBuilder body        = new StringBuilder();
+                body.append("SqlSession sqlSession = sessionFactory.openSession();\r\n");
+                if (each.getReturnType() == void.class)
+                {
+                    ;
+                }
+                else
+                {
+                    body.append("return ");
+                }
+                body.append("sqlSession.getMapper(").append(referenceName).append(".class).").append(each.getName()).append('(');
+                Class<?>[] parameterTypes = each.getParameterTypes();
+                boolean    hasComma       = false;
+                for (int i = 0; i < parameterTypes.length; i++)
+                {
+                    hasComma = true;
+                    body.append("$").append(i).append(',');
+                }
+                if (hasComma)
+                {
+                    body.setLength(body.length() - 1);
+                }
+                body.append(");\r\n");
+                methodModel.setBody(body.toString());
+                classModel.putMethodModel(methodModel);
             }
-            else
-            {
-                body.append("return ");
-            }
-            body.append("sqlSession.getMapper(").append(referenceName).append(".class).").append(each.getName()).append('(');
-            Class<?>[] parameterTypes = each.getParameterTypes();
-            boolean    hasComma       = false;
-            for (int i = 0; i < parameterTypes.length; i++)
-            {
-                hasComma = true;
-                body.append("$").append(i).append(',');
-            }
-            if (hasComma)
-            {
-                body.setLength(body.length() - 1);
-            }
-            body.append(");\r\n");
-            methodModel.setBody(body.toString());
-            classModel.putMethodModel(methodModel);
+            type = type.getSuperclass();
         }
         try
         {
